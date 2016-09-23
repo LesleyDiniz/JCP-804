b411276e2b5f52158909dea6184c7bdbd52f53bf
Lesley Almeida Diniz <ldiniz@scopus.com.br>
2016-09-23 16:50:48 -0300

    Manipulando Strings

--- src/com/br/diniz/jcp804/section5/stringProcessing/Leasson1.java
+++ src/com/br/diniz/jcp804/section5/stringProcessing/Leasson1.java
@@ -1,9 +1,33 @@
 package com.br.diniz.jcp804.section5.stringProcessing;
 
 public class Leasson1 {
-	
-	public static void main(String a[]) {
-		
-	}
 
+	public static void main(String a[]) {
+		String palindrome = "TESTE";
+		int len = palindrome.length();
+		char[] tempCharArray = new char[len];
+		char[] charArray = new char[len];
+		
+		for(int i = len; i > 0; i--) {
+			tempCharArray[len - i] = palindrome.charAt(i-1);
+		}
+		System.out.println(new String(tempCharArray));
+
+		// put original string in an
+		// array of chars
+		for (int i = 0; i < len; i++) {
+			tempCharArray[i] = palindrome.charAt(i);
+		}
+
+		// reverse array of chars
+		for (int j = 0; j < len; j++) {
+			charArray[j] = tempCharArray[len - 1 - j];
+		}
+
+		String reversePalindrome = new String(charArray);
+		System.out.println(reversePalindrome);
+		
+		palindrome.getChars(0, len, tempCharArray, 0);
+		System.out.println(tempCharArray);
+	}
 }
